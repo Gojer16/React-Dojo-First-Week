66a5856ae2b64d28ea10f24fecd0c8aa4f192b97
Gojer <gojer@naver.com>
2025-08-09 09:11:59 -0400

    UI improvement

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -49,67 +49,58 @@ const filteredTodos = todos.filter(todo => {
 
 
   return (
-    <div className='min-h-screen px-4 py-8 bg-gradient-to-br font-medium from-pink-300 via-indigo-400 to-fuchsia-500'>
-  <div className='space-x-6'>
-  <input
-  type="text" 
-  name="addTast" 
-  id="addTask"
-  placeholder='Add Tasks / Activity'
-  required
-  value={newTodos}
-  onKeyDown={(e) => {
-  if (e.key === "Enter") addHandler();
-  }}
-  onChange={(e) => setNewTodos(e.target.value)}
-  className='px-6 py-3 shadow-lg p-3 rounded-4xl text-gray-800 text-center item-center bg-white '
-  />    
-  <button 
-  onClick={addHandler}
-  className='px-4 py-3  shadow-lg rounded-4xl cursor-pointer  text-center item-cente bg-gray-200'>Add Task</button>
-  </div>
+    <div className=' min-h-screen px-4 py-8 bg-gradient-to-br font-medium from-pink-300 via-indigo-400 to-fuchsia-500'>
+      <div className='space-x-6 flex items-center justify-center py-8'>
+        <input
+          type="text"
+          name="addTask"
+          id="addTask"
+          placeholder='Add Tasks / Activity'
+          required
+          value={newTodos}
+          onKeyDown={(e) => {
+          if (e.key === "Enter") addHandler();
+          }}
+          onChange={(e) => setNewTodos(e.target.value)}
+          className='px-6 py-3 shadow-lg p-3 rounded-2xl text-gray-800 text-center item-center bg-white '
+        />    
+        <button 
+          onClick={addHandler}
+          className='outline-none border-none hover:text-white text-base ease-linear relative hover:bg-todo-primary px-8 py-3 hover:shadow-todo-primary hover:-translate-y-1 transform transition-all duration-300 shadow-xl rounded-2xl cursor-pointer  text-center item-center bg-gray-300'>Add Task</button>
+      </div>
     
-   <div className="flex gap-4 mt-6">
-  <button
-    onClick={() => setFilter("all")}
-    className={`px-4 py-2 rounded ${filter === "all" ? "bg-white text-black" : "bg-gray-200"}`}
-  >
-    All
-  </button>
-  <button
-    onClick={() => setFilter("active")}
-    className={`px-4 py-2 rounded ${filter === "active" ? "bg-white text-black" : "bg-gray-200"}`}
-  >
-    Active
-  </button>
-  <button
-    onClick={() => setFilter("completed")}
-    className={`px-4 py-2 rounded ${filter === "completed" ? "bg-white text-black" : "bg-gray-200"}`}
-  >
-    Completed
-  </button>
-</div> 
+   <div className="flex gap-4 py-8 justify-center items-center bg-white/50 rounded-2xl shadow-lg mx-auto">
+      <button
+        onClick={() => setFilter("all")}
+        className={`px-5 py-3 cursor-pointer shadow-xl outline-none border-none hover:shadow-lg transition-all duration-300  ease-linear relative transform hover:scale-105 rounded-2xl ${filter === "all" ? "bg-white hover:bg-gray-100 text-black" : "bg-gray-200"}`}
+      >
+      All
+      </button>
+      <button
+        onClick={() => setFilter("active")}
+        className={`px-6  py-3 cursor-pointer shadow-xl  rounded-2xl  outline-none border-none hover:shadow-lg transition-all duration-300  ease-linear relative transform hover:scale-105 ${filter === "active" ? "bg-white hover:bg-gray-100 text-black" : "bg-gray-200"}`}
+      >
+      Active
+      </button>
+      <button
+        onClick={() => setFilter("completed")}
+        className={`px-6 py-3 cursor-pointer shadow-xl  outline-none border-none hover:shadow-lg transition-all duration-300  ease-linear relative transform hover:scale-105 rounded-2xl ${filter === "completed" ? "bg-white hover:bg-gray-100 text-black" : "bg-gray-200"}`}
+      >
+      Completed
+        </button>
+    </div> 
 
-  <ul className='px-4 py-12 flex items-center text-center gap-4'>
-    {filteredTodos.map((item) => (
-      <TodoItem
-      key={item.id}
-      item={item} 
-      onDelete={deleteHandler} 
-      onToggle={toggleComplete} />
-    ))}
-  </ul>
+      <ul className='px-4 py-12 flex justify-start items-start min-h-screen text-center gap-4'>
+        {filteredTodos.map((item) => (
+        <TodoItem
+        key={item.id}
+        item={item} 
+        onDelete={deleteHandler} 
+        onToggle={toggleComplete} />
+        ))}
+      </ul>
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    </div>
+  </div>
   )
 }
 
--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -2,16 +2,16 @@
 
 const TodoItem = ({ item, onDelete, onToggle }) => {
   return (
-    <li className='hover:-translate-y-1.5 hover:shadow-2xl bg-white flex gap-4 text-center items-center shadow-md rounded-lg p-4'>
+    <li className='hover:-translate-y-1.5 hover:shadow-2xl bg-white flex gap-4 text-center items-center shadow-md rounded-2xl p-4'>
       {item.text}
       <button
-        className='bg-indigo-200 rounded-2xl text-center cursor-pointer  px-4 py-2'
+        className='bg-todo-delete rounded-2xl  text-white text-center cursor-pointer  px-4 py-2'
         onClick={() => onDelete(item.id)}
         >
         Delete
         </button>
       <button
-        className={`${item.completed ? 'bg-red-500' : 'bg-green-500'} rounded-2xl text-center cursor-pointer  px-4 py-2`}
+        className={`${item.completed ? 'bg-todo-undo' : 'bg-todo-complete'} rounded-2xl  text-center cursor-pointer  px-4 py-2`}
         onClick={() => onToggle(item.id)}
         >
         {item.completed ? 'Undo' : 'Complete'}
